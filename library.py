0e91a3cefc92eebfdb6bbdfe4d331a1c69fc0bcb
louWertman <lwertman@tuta.io>
2024-11-25 09:19:45 -0500

    Added Function outline and documentation for library.py

--- library.py
+++ library.py
@@ -2,6 +2,46 @@
 
 #library
 
+# input: pathString - path to file
+# return: bool - true or false if the file exist, will return false if permission is denied
+def doesExist(pathString):
+    pass
+
+# input: pathString - file to covert to []byte
+# return:  []byte data of file
+def fileToByte(file):
+    pass
+
+# input: path - path to return directory from
+# return: string - directory where the file resides
+def returnDirectory(path):
+    pass
+
+# input: path - where to create directory
+# return: bool - success
+def createDirectory(path):
+    pass
+
+# input: buffer - recieved buffer; filePath - where to create file
+# return: bool - true or false if the file exist
+def bufferToFile(buffer, filePath):
+    pass
+
+# input: path - path to the file string
+# return: bool - file created successfully
+def createFile(path):
+    pass
+
+# input: commandString - command to execute
+# return: int - return value of execution
+def execBash(commandString):
+    pass
+
+# input: filePath
+# return: string - bash commands to copy directory
+def directoryCopy(filePath):
+    pass
+
 def main():
     pass
 
