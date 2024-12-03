123d9a1f97ab21bfc3d801d0afb6507093966a3c
louWertman <lwertman@tuta.io>
2024-12-03 10:31:29 -0500

    some handler stuff

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 import argparse
 import library
+import os
 
 #client
 
@@ -37,10 +38,45 @@ def replLOOP():
 #purpose: to process the logic of the user argument and package data to send to the server
 def handler(userInput):
     userRequest = library.replParse(str(userInput))
-    if(userRequest.baseCMD == "quit"):
+    baseCMD = userRequest.baseCMD
+    if(baseCMD == "quit"):
         exit(0)
-        if(user):
+    elif(baseCMD == "help"):
+        printHelp()
+    
 
+#purpose: print the massive help string, just in a seperate function for neatness
+def printHelp():
+    helpString = """
+    exit – quit the application.
+
+    cd [path] – Change remote directory to path. If path is not specified, then change directory to the one the session started in.
+
+    get [-R] remote-path [local-path] – Retrieve the remote-path and store it on the local machine. If the local path name is not specified, it is given the same name it has on the remote machine.
+
+    If the -R flag is specified then directories will be copied recursively.
+
+    help – Display help text.
+
+    lcd [path] – Change local directory to path. If path is not specified, then change directory to the local user’s home directory.
+
+    lls [path] – Display local directory listing of either path or current directory if path is not specified.
+
+    lmkdir path – Create local directory specified by path.
+
+    lpwd – Print local working directory.
+
+    ls [path]Display a remote directory listing of either path or the current directory ifpath` is not specified.
+
+    mkdir path – Create remote directory specified by path.
+
+    put [-R] local-path [remote-path] – Upload local-path and store it on the remote machine. If the remote path name is not specified, it is given the same name it has on the local machine.
+
+    If the -R flag is specified then directories will be copied recursively.
+
+    pwd - Display remote working directory.
+    """
+    print(helpString)
 
 
 def main():
